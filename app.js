df3a39afc036dce6870ff78b4933b4b48073a5e8
terenzzzz <terence592592@gmail.com>
2022-09-09 14:25:19 +0100

    port

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ app.use('/api', functionsRouter)
 
 // write your code here...
 // 调用 app.listen 方法，指定端口号并启动web服务器 
-app.listen(3007, function () {
-    console.log('api server running at https://eleme-node.herokuapp.com:3007')
+app.listen(592592, function () {
+    console.log('api server running at https://eleme-node.herokuapp.com:592592')
 })
--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ const mysql = require('mysql')
 const db = mysql.createPool({
     // host: '192.168.3.36',
     host: 'xs47628438.oicp.vip',
+    port:'24706',
     user: 'terence',
     password: '592592',
     database:'eleme'

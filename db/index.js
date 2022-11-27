d6ee27915dbf1453463b78e818d6e5cb50082859
terenzzzz <terence592592@gmail.com>
2022-11-27 02:14:34 +0000

    dbServer

--- db/index.js
+++ db/index.js
@@ -4,11 +4,11 @@ const mysql = require('mysql')
 const db = mysql.createPool({
     // 开发使用
     // host: '192.168.3.36',
-    host: '8.134.128.178',
+    host: '46.101.60.239',
     //部署使用
     // host: 'xs47628438.oicp.vip',
     // port:'24706',
-    user: 'nodejs',
+    user: 'server',
     password: '592592',
     database:'eleme'
 })
